0e02983e8d76460f28134b8fa28c113d5a28f662
Julia <mukhammadieva.julia@gmail.com>
2021-06-19 18:32:23 +0200

    Added List of Restaurant

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFoodData;
+using System.Collections.Generic;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            _config = config;
+            _restaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+            Message = _config["Message"];
+            Restaurants = _restaurantData.GetAll();
+        }
+    }
+}
---
+++ OdeToFoodData/IRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFoodData
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Pizza", Location = "Prague 10, Francouzska, 489/85", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "U Parashutista", Location = "Prague 2, Karlova, 1089/87", Cuisine = CuisineType.Czech},
+                new Restaurant {Id = 3, Name = "New Mexico", Location = "Prague 6, Litova, 12/12", Cuisine = CuisineType.Mexican},
+                new Restaurant {Id = 4, Name = "APOLO", Location = "Prague 1, Naplovka, 5", Cuisine = CuisineType.None},
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            // return from r in restaurants
+            // orderby r.Name
+            // select r;
+            List<Restaurant> newRestaurants = restaurants.OrderBy(r => r.Name).ToList();
+            return newRestaurants;
+        }
+    }
+}
